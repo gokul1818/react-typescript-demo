e0bb87fa3c3f0e0257815f63b8dd5e100691f3a9
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2024-04-26 08:58:12 +0530

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,56 @@
-import './App.css';
-import { BoldStyle } from './components/boldStyle';
-import { Button } from './components/button';
-import { Heading } from './components/childrenProps';
-import { Counter } from './components/counter';
-import { Greet } from './components/greet';
-import { Input } from './components/input';
-import { Status } from './components/status';
+import "./App.css";
+import { BoldStyle } from "./components/boldStyle";
+import { Button } from "./components/button";
+import { Heading } from "./components/childrenProps";
+import { Counter } from "./components/counter";
+import { Greet } from "./components/greet";
+import { Input } from "./components/input";
+import { Status } from "./components/status";
 
 function App() {
-
   const personObj = {
     firstName: "sowmiya",
-    lastName: "G"
-  }
+    lastName: "G",
+  };
 
   const arrayOfObject = [
     {
       firstName: "sowmiya",
-      lastName: "G"
+      lastName: "G",
     },
     {
       firstName: "gokul",
-      lastName: "B"
+      lastName: "B",
     },
     {
       firstName: "Krish",
-      lastName: "nan"
+      lastName: "nan",
     },
-  ]
+  ];
+  
+
   return (
     <div className="App">
       <BoldStyle>
         <Greet name={"hi Sowmiya"} active={true} personObj={personObj} />
         <Heading>
-          <Status status='pending' />
-          hi sowiya hfyo rouqoiruqoiwuoiqwuroiquroiqeurouo</Heading>
+          <Status status="pending" />
+          Hi Sowmiya hfyo rouqoiruqoiwuoiqwuroiquroiqeurouo
+        </Heading>
         ajhsdjgasdfh
       </BoldStyle>
-      <Input type="text" value='' handleChange={(x) => console.log(x.target.value)} style={{ color: "red" }} />
-      <Button handleclick={(id) => console.log("button click", id,)} btnLabel="Click" id={2} />
+      <Input
+        type="text"
+        value=""
+        handleChange={(x) => console.log(x.target.value)}
+        style={{ color: "red" }}
+      />
+      <Button
+        handleclick={(id) => console.log("button click", id)}
+        btnLabel="Click"
+        id={2}
+      />
       <Counter />
-
     </div>
   );
 }
--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,34 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { Button } from "./button";
 
+type stateProps = {
+  count: number;
+};
+
+type actionProps = {
+  type: string;
+  payload: number;
+};
+
+type actionsProps = {
+  type: "add" | "minus" ;
+  payload: number;
+};
+
+
+type actionsProps2 = {
+  type: "reset" ;
+}; 
+
+type actionProps3 =  actionsProps | actionsProps2
+
+const initialState = { count: 0 };
+
+function reducer(state: stateProps, action: actionProps3) {
+  switch (action.type) {
+    case "add":
+      return { count: state.count + action.payload};
+    case "minus":
+      return { count: state.count - action.payload };
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+}
+
+const name: any = "sowmi"
+const result  =  name as string
+console.log(result)
+
 export const Counter = () => {
-    const [currentNumber, setCurrentNumber] = useState<number | null>(0);
+  const [currentNumber, setCurrentNumber] = useState<number | null>(0);
 
-    const handleIncrement = () => {
-        if (currentNumber !== null) {
-            setCurrentNumber(currentNumber + 1);
-        }
-        else{
-            setCurrentNumber(0)
-        }
-    };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const handleIncrement = () => {
+    if (currentNumber !== null) {
+      setCurrentNumber(currentNumber + 1);
+    } else {
+      setCurrentNumber(0);
+    }
+  };
 
-    const handleDecrement = () => {
-        if (currentNumber !== null) {
-            setCurrentNumber(currentNumber - 1);
-        }
-    };
+  const handleDecrement = () => {
+    if (currentNumber !== null) {
+      setCurrentNumber(currentNumber - 1);
+    }
+  };
 
-    const handleReset = () => {
-        setCurrentNumber(0);
-    };
+  const handleReset = () => {
+    setCurrentNumber(0);
+  };
 
-    return (
-        <div style={{ display: "flex" }}>
-            <Button handleclick={handleIncrement} btnLabel="Increment" />
-            <p>{currentNumber}</p>
-            <Button handleclick={handleDecrement} btnLabel="Decrement" />
-            <Button handleclick={() => setCurrentNumber(null)} btnLabel="Reset" />
-        </div>
-    );
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <Button handleclick={handleIncrement} btnLabel="Increment" />
+        <p>{currentNumber}</p>
+        <Button handleclick={handleDecrement} btnLabel="Decrement" />
+        <Button handleclick={() => setCurrentNumber(null)} btnLabel="Reset" />
+      </div>
+      {/* use Reducer */}
+      <div style={{ display: "flex" }}>
+        <p>COUNT: {state.count}</p>
+        <Button
+          handleclick={() => dispatch({ type: "add", payload: 2 })}
+          btnLabel="ADD"
+        />
+        <Button
+          handleclick={() => dispatch({ type: "minus", payload: 2 })}
+          btnLabel="MINUS"
+        />
+        <Button
+          handleclick={() => dispatch({ type: "reset" })}
+          btnLabel="RESET"
+        />
+      </div>
+    </>
+  );
 };
